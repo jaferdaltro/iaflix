e3ff0d4ccb33f58c2e50340de59c3980158602b3
Jafer Daltro <jafer.daltro@gmail.com>
2024-01-13 08:58:02 -0300

    Add movie details.

--- app/router.js
+++ app/router.js
@@ -8,5 +8,5 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('movies', { path: '/' });
-  this.route('movies/details', { path: ':movie_id' });
+  this.route('movies/details', { path: '/:movie_id' });
 });
